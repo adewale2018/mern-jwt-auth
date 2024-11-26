eb5678649fbd010fa76891af319f10e2576fc1d3
Saheed Adewale Shittu <saheedadewaleshittu@gmail.com>
2024-11-26 17:40:50 +0100

    Implement logout endpoint

--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -1,9 +1,11 @@
 import { CREATED, OK } from "../constants/http";
+import { clearAuthCookies, setAuthCookies } from "../utils/cookies";
 import { createAccount, loginUser } from "../services/auth.service";
 import { loginSchema, registerSchema } from "./auth.schemas";
 
 import { catchErrors } from "../utils/catchErrors";
-import { setAuthCookies } from "../utils/cookies";
+import sessionModel from "../models/session.model";
+import { verifyToken } from "../utils/jwt";
 
 export const registerHandler = catchErrors(async (req, res) => {
   // validate request
@@ -28,5 +30,16 @@ export const loginHandler = catchErrors(async (req, res) => {
   const { accessToken, refreshToken } = await loginUser(request);
   return setAuthCookies({ res, accessToken, refreshToken }).status(OK).json({
     message: "Login successful",
+  });
+});
+
+export const logoutHandler = catchErrors(async (req, res) => {
+  const accessToken = req.cookies.accessToken;
+  const { payload } = verifyToken(accessToken);
+  if (payload) {
+    await sessionModel.findByIdAndDelete(payload.sessionId);
+  }
+  return clearAuthCookies(res).status(OK).json({
+    message: "Logout successful!",
   });
 });
--- backend/src/utils/jwt.ts
+++ backend/src/utils/jwt.ts
@@ -1,5 +1,5 @@
 import { JWT_REFRESH_SECRET, JWT_SECRET } from "../constants/env";
-import jwt, { SignOptions } from "jsonwebtoken";
+import jwt, { SignOptions, VerifyOptions } from "jsonwebtoken";
 
 import { SessionDocument } from "../models/session.model";
 import { UserDocument } from "../models/user.model";
@@ -37,4 +37,21 @@ export const signToken = (
 ) => {
   const { secret, ...signOpts } = options || accessTokenSignOptions;
   return jwt.sign(payload, secret, { ...defaults, ...signOpts });
+};
+
+export const verifyToken = <TPayload extends object = AccessTokenPayload>(
+  token: string,
+  options?: VerifyOptions & { secret: string }
+) => {
+  const { secret = JWT_SECRET, ...verifyOpts } = options || {};
+  try {
+    const payload = jwt.verify(token, secret, {
+      ...defaults,
+      ...verifyOpts,
+    }) as TPayload;
+
+    return { payload };
+  } catch (error: any) {
+    return { error: error.message };
+  }
 };
