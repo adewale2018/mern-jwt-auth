e1f9172f07d0128ac45c20a0a0073dc1be54127a
Saheed Adewale Shittu <saheedadewaleshittu@gmail.com>
2024-11-26 14:46:12 +0100

    Implement login endpoint

--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -1,19 +1,9 @@
-import { CREATED } from "../constants/http";
+import { CREATED, OK } from "../constants/http";
+import { createAccount, loginUser } from "../services/auth.service";
+import { loginSchema, registerSchema } from "./auth.schemas";
+
 import { catchErrors } from "../utils/catchErrors";
-import { createAccount } from "../services/auth.service";
 import { setAuthCookies } from "../utils/cookies";
-import { z } from "zod";
-const registerSchema = z
-  .object({
-    email: z.string().email().min(1).max(255),
-    password: z.string().min(6).max(255),
-    confirmPassword: z.string().min(6).max(255),
-    userAgent: z.string().optional(),
-  })
-  .refine((data) => data.password === data.confirmPassword, {
-    message: "Passwords do not match",
-    path: ["confirmPassword"],
-  });
 
 export const registerHandler = catchErrors(async (req, res) => {
   // validate request
@@ -27,4 +17,16 @@ export const registerHandler = catchErrors(async (req, res) => {
   return setAuthCookies({ res, accessToken, refreshToken })
     .status(CREATED)
     .json(user);
+});
+
+export const loginHandler = catchErrors(async (req, res) => {
+  const request = loginSchema.parse({
+    ...req.body,
+    userAgent: req.headers["user-agent"],
+  });
+  // call service
+  const { accessToken, refreshToken } = await loginUser(request);
+  return setAuthCookies({ res, accessToken, refreshToken }).status(OK).json({
+    message: "Login successful",
+  });
 });
--- backend/src/services/auth.service.ts
+++ backend/src/services/auth.service.ts
@@ -1,6 +1,7 @@
-import { JWT_REFRESH_SECRET, JWT_SECRET } from "../constants/env";
+import { CONFLICT, UNAUTHORIZED } from "../constants/http";
+import { refreshTokenSignOptions, signToken } from "../utils/jwt";
 
-import { CONFLICT } from "../constants/http";
+import { JWT_REFRESH_SECRET } from "../constants/env";
 import UserModel from "../models/user.model";
 import VerificationCodeType from "../constants/verificationCodeTypes";
 import appAssert from "../utils/appAssert";
@@ -19,45 +20,81 @@ export const createAccount = async (data: CreateAccountParams) => {
   // verify that existing user does not already exist
   const existingUser = await UserModel.exists({ email: data.email });
   appAssert(!existingUser, CONFLICT, "Email already exists!");
-  // if (existingUser) {
-  //   throw new Error("User already exists!");
-  // }
   // create the new user
   const user = await UserModel.create({
     email: data.email,
     password: data.password,
   });
+  
+  const userId = user._id;
+  
   // create verification code
   const verificationCode = await verificationCodeModel.create({
-    userId: user._id,
+    userId,
     type: VerificationCodeType.EmailVerification,
     expiresAt: oneYearFromNow(),
   });
   // send verification email with the code
   // create session
   const session = await sessionModel.create({
-    userId: user._id,
+    userId,
     userAgent: data.userAgent,
   });
   // sign access token and the refresh token
-  const refreshToken = jwt.sign(
+  const refreshToken = signToken(
     { sessionId: session._id },
-    JWT_REFRESH_SECRET,
-    {
-      audience: ["user"],
-      expiresIn: "30d",
-    }
+    refreshTokenSignOptions
   );
 
+  jwt.sign({ sessionId: session._id }, JWT_REFRESH_SECRET, {
+    audience: ["user"],
+    expiresIn: "30d",
+  });
+
   // sign access token
-  const accessToken = jwt.sign(
-    { userId: user._id, sessionId: session._id },
-    JWT_SECRET,
-    {
-      audience: ["user"],
-      expiresIn: "15m",
-    }
-  );
+  const accessToken = signToken({
+    userId,
+    sessionId: session._id,
+  });
+
+  // return user and tokens
+  return { user: user.omitPassword(), accessToken, refreshToken };
+};
+
+export type LoginParams = {
+  email: string;
+  password: string;
+  userAgent?: string;
+};
+
+export const loginUser = async ({
+  email,
+  password,
+  userAgent,
+}: LoginParams) => {
+  // get the user by email
+  const user = await UserModel.findOne({ email });
+  appAssert(user, UNAUTHORIZED, "Invalid email or password!");
+  // validate password from the request
+  const isValid = await user.comparePassword(password);
+  appAssert(isValid, UNAUTHORIZED, "Invalid email or password!");
+  // create a new session
+  const userId = user._id;
+  const session = await sessionModel.create({
+    userId,
+    userAgent,
+  });
+  const sessionInfo = {
+    sessionId: session._id,
+  };
+  // sign access token and the refresh token
+  const refreshToken = signToken(sessionInfo, refreshTokenSignOptions);
+
+  // sign access token
+  const accessToken = signToken({
+    userId,
+    ...sessionInfo,
+  });
   // return user and tokens
   return { user: user.omitPassword(), accessToken, refreshToken };
 };
